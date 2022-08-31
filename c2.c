847d0e8015c908cf95aef930ad9730bf8c30f0bc
Omprakash Banerjee <omprakashbanerjeee@gmail.com>
2022-08-31 09:07:09 +0530

    minor changes

--- c2.c
+++ c2.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include<limits.h>
-void main()
+int main()
 {
     int i;
 char var=65;
 int var2=INT_MAX;
 for(i=0;i<=255;i++)
     {
-        printf("%d is equivalent to %c\n",var,var);
+        printf("%d is equivalent to %c\n",var,var2);
         var=var++;
 }
 return 0;
